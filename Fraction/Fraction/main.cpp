8dc7314bb9c26e79e23753ff53b84d2d2e7f480b
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-08-23 09:33:09 +0300

    23.08.2022

--- Fraction/Fraction/main.cpp
+++ Fraction/Fraction/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Fraction {
+
 public:
 	Fraction()
 	{
@@ -75,7 +76,32 @@ public:
 		this->numerator += this->denominator;
 		return oldFraction;
 	}
-	void shorten() {
+	void Pow(int __pow) {
+		if (__pow == 0) {
+			this->numerator = this->denominator = 1;
+			return;
+		}
+		if (__pow < 0) {
+			__pow *= -1;
+			int __temp = this->numerator;
+			this->numerator = this->denominator;
+			this->denominator = __temp;
+		}
+			int __temp_numerator = 1;
+			int __temp_denominator = 1;
+		do {
+			__temp_denominator *= this->denominator;
+			__temp_numerator *= this->numerator;
+			--__pow;
+		} while (__pow != 0);
+		this->numerator = __temp_numerator;
+		this->denominator = __temp_denominator;
+	}
+	void SquareRoot() {
+		this->numerator = sqrt(this->numerator);
+		this->denominator = sqrt(this->denominator);
+	}
+	void reduction() {
 		if (this->numerator == this->denominator) {
 			this->integral = 1;
 			this->numerator = 0;
@@ -101,7 +127,19 @@ public:
 		return false;
 	}
 	bool operator<(Fraction& other) {
-		return !(*this > other);
+		if (this->numerator * other.denominator < this->denominator * other.numerator)
+			return true;
+		return false;
+	}
+	bool operator>=(Fraction& other) {
+		if (this->numerator * other.denominator >= this->denominator * other.numerator)
+			return true;
+		return false;
+	}
+	bool operator<=(Fraction& other) {
+		if (this->numerator * other.denominator <= this->denominator * other.numerator)
+			return true;
+		return false;
 	}
 	void print() {
 		if (this->denominator < 0) {
@@ -137,27 +175,38 @@ private:
 
 
 int main() {
-	Fraction A;
-	A.setNumerator(21);
-	A.setDenominator(84);
-	Fraction B(49,133);
-	Fraction C = A + B;
-	Fraction D = A - B;
-	Fraction E = A * B;
-	Fraction F = A / B;
-	Fraction G = A++;
-	Fraction H = ++B;
-	cout << "A = "; A.print(); A.shorten(); cout << "--->"; A.print(); cout << endl;
-	cout << "B = "; B.print(); B.shorten(); cout << "--->"; B.print(); cout << endl;
-	cout << "C = "; C.print(); C.shorten(); cout << "--->"; C.print(); cout << endl;
-	cout << "D = "; D.print(); D.shorten(); cout << "--->"; D.print(); cout << endl;
-	cout << "E = "; E.print(); E.shorten(); cout << "--->"; E.print(); cout << endl;
-	cout << "F = "; F.print(); F.shorten(); cout << "--->"; F.print(); cout << endl;
-	cout << "G = "; G.print(); G.shorten(); cout << "--->"; G.print(); cout << endl;
-	cout << "H = "; H.print(); H.shorten(); cout << "--->"; H.print(); cout << endl;
-	cout << "D < E" << ((D < E) ? " true" : " false") << endl;
-	cout << "G > C" << ((G > C) ? " true" : " false") << endl;
-	
+
+	Fraction A;   // calling default constructor
+	A.setNumerator(21); // calling setter for numerator
+	A.setDenominator(84); // calling setter for denominator
+	Fraction B(49,133); // calling constructor with parameters
+	Fraction C = A + B; // calling copy constructor after getting result of '+'
+	Fraction D = A - B; // calling copy constructor after getting result of '-'
+	Fraction E = A * B; // calling copy constructor after getting result of '*'
+	Fraction F = A / B; // calling copy constructor after getting result of '/'
+	Fraction G = A++;	// calling copy constructor after postfix-incrementing 'A'
+	Fraction H = ++B;	// calling copy constructor after prefix-incrementing 'H'
+	Fraction I(3, 7);	// calling constructor with parameters
+	I.Pow(3);			// Fraction I^3
+	Fraction J(4, 144);	// calling constructor with parameters
+	J.SquareRoot();		// Sqrt(J)
+
+	cout << "A = "; A.print(); A.reduction(); cout << "--->"; A.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "B = "; B.print(); B.reduction(); cout << "--->"; B.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "C = "; C.print(); C.reduction(); cout << "--->"; C.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "D = "; D.print(); D.reduction(); cout << "--->"; D.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "E = "; E.print(); E.reduction(); cout << "--->"; E.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "F = "; F.print(); F.reduction(); cout << "--->"; F.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "G = "; G.print(); G.reduction(); cout << "--->"; G.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "H = "; H.print(); H.reduction(); cout << "--->"; H.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "I = "; I.print(); I.reduction(); cout << "--->"; I.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << "J = "; J.print(); J.reduction(); cout << "--->"; J.print(); cout << endl; // printing fractions with reduction and defining integral part
+	cout << boolalpha;							// for printing boolean values as "true" of "false", not '0' or '1'
+	cout << "D < E - " << (D < E) << endl;		// fraction comparison
+	cout << "G > C - " << (G > C) << endl;		// fraction comparison
+	cout << "F >= H - " << (F >= H) << endl;	// fraction comparison
+	cout << "A <= B - " << (A <= B) << endl;	// fraction comparison
+
 	return 0;
 
-}
+};
