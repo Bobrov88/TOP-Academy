c4db70dec6ac612cf624f4d7cfdb1245c9aeb177
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-08-23 09:39:55 +0300

    complete!

--- Fraction/Fraction/main.cpp
+++ Fraction/Fraction/main.cpp
@@ -176,20 +176,20 @@ private:
 
 int main() {
 
-	Fraction A;   // calling default constructor
-	A.setNumerator(21); // calling setter for numerator
-	A.setDenominator(84); // calling setter for denominator
-	Fraction B(49,133); // calling constructor with parameters
-	Fraction C = A + B; // calling copy constructor after getting result of '+'
-	Fraction D = A - B; // calling copy constructor after getting result of '-'
-	Fraction E = A * B; // calling copy constructor after getting result of '*'
-	Fraction F = A / B; // calling copy constructor after getting result of '/'
-	Fraction G = A++;	// calling copy constructor after postfix-incrementing 'A'
-	Fraction H = ++B;	// calling copy constructor after prefix-incrementing 'H'
-	Fraction I(3, 7);	// calling constructor with parameters
-	I.Pow(3);			// Fraction I^3
-	Fraction J(4, 144);	// calling constructor with parameters
-	J.SquareRoot();		// Sqrt(J)
+	Fraction A;					// calling default constructor
+	A.setNumerator(21);			// calling setter for numerator
+	A.setDenominator(84);		// calling setter for denominator
+	Fraction B(49,133);			// calling constructor with parameters
+	Fraction C = A + B;			// calling copy constructor after getting result of '+'
+	Fraction D = A - B;			// calling copy constructor after getting result of '-'
+	Fraction E = A * B;			// calling copy constructor after getting result of '*'
+	Fraction F = A / B;			// calling copy constructor after getting result of '/'
+	Fraction G = A++;			// calling copy constructor after postfix-incrementing 'A'
+	Fraction H = ++B;			// calling copy constructor after prefix-incrementing 'H'
+	Fraction I(3, 7);			// calling constructor with parameters
+	I.Pow(3);					// Fraction I^3
+	Fraction J(4, 144);			// calling constructor with parameters
+	J.SquareRoot();				// Sqrt(J)
 
 	cout << "A = "; A.print(); A.reduction(); cout << "--->"; A.print(); cout << endl; // printing fractions with reduction and defining integral part
 	cout << "B = "; B.print(); B.reduction(); cout << "--->"; B.print(); cout << endl; // printing fractions with reduction and defining integral part
