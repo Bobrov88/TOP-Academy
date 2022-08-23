52a4fd9c923db527eeedfd94f1454d0dd5ca507a
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-08-23 09:40:29 +0300

    Done!

--- Fraction/Fraction/main.cpp
+++ Fraction/Fraction/main.cpp
@@ -178,7 +178,7 @@ int main() {
 
 	Fraction A;					// calling default constructor
 	A.setNumerator(21);			// calling setter for numerator
-	A.setDenominator(84);		// calling setter for denominator
+	A.setDenominator(84);	// calling setter for denominator
 	Fraction B(49,133);			// calling constructor with parameters
 	Fraction C = A + B;			// calling copy constructor after getting result of '+'
 	Fraction D = A - B;			// calling copy constructor after getting result of '-'
@@ -187,7 +187,7 @@ int main() {
 	Fraction G = A++;			// calling copy constructor after postfix-incrementing 'A'
 	Fraction H = ++B;			// calling copy constructor after prefix-incrementing 'H'
 	Fraction I(3, 7);			// calling constructor with parameters
-	I.Pow(3);					// Fraction I^3
+	I.Pow(3);				// Fraction I^3
 	Fraction J(4, 144);			// calling constructor with parameters
 	J.SquareRoot();				// Sqrt(J)
 
