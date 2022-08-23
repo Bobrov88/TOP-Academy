ef288f2feb158e87dd8fbb3d3704a4bce26be991
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-08-23 09:41:28 +0300

    23.08.2022 done!

--- Fraction/Fraction/main.cpp
+++ Fraction/Fraction/main.cpp
@@ -176,7 +176,7 @@ private:
 
 int main() {
 
-	Fraction A;					// calling default constructor
+	Fraction A;				// calling default constructor
 	A.setNumerator(21);			// calling setter for numerator
 	A.setDenominator(84);			// calling setter for denominator
 	Fraction B(49,133);			// calling constructor with parameters
