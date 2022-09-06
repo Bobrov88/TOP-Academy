6ec963d1d7d51b4e4d4f47c5eb5beaa7a7aaba8d
bobrov.dmitriy@mail.ru <bobrov.dmitriy@mail.ru>
2022-09-06 16:24:16 +0300

    06.09.2022 delete

--- Reservoir/Reservoir/includes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include "Interface.h"
-#include "reservoir.h"
-constexpr auto KOEFF{ 0.7 };
--- Reservoir/Reservoir/main.cpp
+++ Reservoir/Reservoir/main.cpp
@@ -1,5 +1,3 @@
-//#include "includes.h"
-
 #include <iostream>
 #include <string>
 constexpr auto KOEFF{ 0.7 };
